e65ebba346b312133a1fcba8a4a36ae8d1287f01
trailbrakevisuals <milandekkersbusiness@gmail.com>
2026-09-19 02:29:22 +0200

    Add Trailbrake Visuals metadata system

---
+++ js/metadata.js
@@ -0,0 +1,28 @@
+window.TrailbrakeMetadata = {
+
+    defaultEvent: "NLS8&9",
+
+    events: {
+
+        "NLS8": {
+            name: "NLS 8",
+            date: "12 September 2026",
+            location: "Nürburgring Nordschleife"
+        },
+
+        "NLS9": {
+            name: "NLS 9",
+            date: "13 September 2026",
+            location: "Nürburgring Nordschleife"
+        },
+
+        "NLS8&9": {
+            name: "NLS 8 & 9",
+            date: "12 + 13 September 2026",
+            location: "Nürburgring Nordschleife"
+        }
+    },
+
+    photos: {}
+
+};
