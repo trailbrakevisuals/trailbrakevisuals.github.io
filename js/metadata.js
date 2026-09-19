7ea8db21799c3da88b3451cbd4488ac32081c11f
trailbrakevisuals <milandekkersbusiness@gmail.com>
2026-09-19 03:46:22 +0200

    Update NLS 8 and 9 metadata
    Removed individual event entries for NLS8 and NLS9.

--- js/metadata.js
+++ js/metadata.js
@@ -4,18 +4,6 @@ window.TrailbrakeMetadata = {
 
     events: {
 
-        "NLS8": {
-            name: "NLS 8",
-            date: "12 September 2026",
-            location: "Nürburgring Nordschleife"
-        },
-
-        "NLS9": {
-            name: "NLS 9",
-            date: "13 September 2026",
-            location: "Nürburgring Nordschleife"
-        },
-
         "NLS8&9": {
             name: "NLS 8 & 9",
             date: "12 + 13 September 2026",
